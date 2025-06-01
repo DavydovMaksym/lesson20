6ca15a844f3f69f360f7084d364291a41a062c68
DavydovMaksym <davyidovmax1703@gmail.com>
2025-06-01 14:59:09 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Mark.hpp"
+
+int main()
+{
+    Student mark;
+    std::cout << student.getFirstName() << ' ' << student.getLastName() << std::endl;
+    student.setFirstName("Thomas").setLastName("Franchesko");
+    std::cout << student.getFirstName() << ' ' << student.getLastName() << std::endl;
+
+    Student student2;
+}
